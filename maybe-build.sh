b48f2347f44f3ca4678e52ddb3d5e4153d2245ff
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-07-19 16:06:03 -0300

    Improve cleanup process

--- maybe-build.sh
+++ maybe-build.sh
@@ -12,12 +12,12 @@ fi
 
 # Cleanup
 rmFind() {
-	find "${1}" -type d -name "${2}" | while read DIR_TO_REMOVE; do
+	find "${1}" -type d -name "${2}" -prune | while read DIR_TO_REMOVE; do
 	echo "Removing ${DIR_TO_REMOVE}"
 	rm -rf "${DIR_TO_REMOVE}"
 	done
 }
-rmFind "${GITHUB_WORKSPACE}/$BUILD_DIR" ".git"
-rmFind "${GITHUB_WORKSPACE}/$BUILD_DIR" ".github"
 rmFind "${GITHUB_WORKSPACE}/$BUILD_DIR" "node_modules"
 rm -rf "${GITHUB_WORKSPACE}/$BUILD_DIR/vendor"
+rmFind "${GITHUB_WORKSPACE}/$BUILD_DIR" ".git"
+rmFind "${GITHUB_WORKSPACE}/$BUILD_DIR" ".github"
