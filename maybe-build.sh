486721c080ecd826eeed287853f7eec26a4b23ce
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-11-15 18:44:19 -0300

    No more cleanup, this will be handled by deployignore

--- maybe-build.sh
+++ maybe-build.sh
@@ -9,15 +9,3 @@ else
 	echo "Running default build script"
 	build-for-deployment.sh
 fi
-
-# Cleanup
-rmFind() {
-	find "${1}" -type d -name "${2}" -prune | while read DIR_TO_REMOVE; do
-	echo "Removing ${DIR_TO_REMOVE}"
-	rm -rf "${DIR_TO_REMOVE}"
-	done
-}
-rmFind "${GITHUB_WORKSPACE}/$BUILD_DIR" "node_modules"
-rm -rf "${GITHUB_WORKSPACE}/$BUILD_DIR/vendor"
-rmFind "${GITHUB_WORKSPACE}/$BUILD_DIR" ".git"
-rmFind "${GITHUB_WORKSPACE}/$BUILD_DIR" ".github"
